1bc04fe382054d858ee880fde3993641c044201b
Juan-Analitcs-Data <hidanakt73@gmail.com>
2026-05-16 11:33:30 -0300

    Aprendendo metodos de adicionar elementos  antes ou  depois de um elemento

--- JavaScipt/CFB Curso/atividades/praticandoFilter/script.js
+++ JavaScipt/CFB Curso/atividades/praticandoFilter/script.js
@@ -3,12 +3,18 @@ const btn_c=[...document.querySelectorAll(".curso")]
 const c1_2=document.querySelector("#c1_2")
 const cursos=["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
 const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
+const btnRemoverCurso = document.getElementById("btnRemoverCurso")
+const btnAdicionarNovoCursoAntes = document.getElementById("btnAdicionarNovoCursoAntes")
+const btnAdicionarNovoCursoDepois = document.getElementById("btnAdicionarNovoCursoDepois")
+const nomeCurso = document.getElementById("nomeCurso")
 
-cursos.map((el,chave)=>{
+let indice = 0
+
+const criarNovoCurso = (curso)=>{
     const novoElemento=document.createElement("div")
-    novoElemento.setAttribute("id","c"+chave)
+    novoElemento.setAttribute("id","c"+indice)
     novoElemento.setAttribute("class","curso c1")
-    novoElemento.innerHTML=el
+    novoElemento.innerHTML=curso
 
     const comandos=document.createElement("div")
     comandos.setAttribute("class","comandos")
@@ -21,24 +27,78 @@ cursos.map((el,chave)=>{
 
     novoElemento.appendChild(comandos)
 
-    caixaCursos.appendChild(novoElemento)
+    return novoElemento
+}
+cursos.map((el,chave)=>{
     
+    const novoElemento = criarNovoCurso(el)
+    caixaCursos.appendChild(novoElemento)
+    indice++
 })
-
-btnCursoSelecionado.addEventListener("click",(evt)=>{
+const radioSelecionado = ()=>{
     const todosRadios=[...document.querySelectorAll("input[type=radio]")]
     let radioSelecionado=todosRadios.filter((ele,ind,arr)=>{
         return ele.checked
     })
-    radioSelecionado=radioSelecionado[0]
-    //const cursoSelecionado=radioSelecionado.parentNode.parentNode.firstChild.textContent
-    const cursoSelecionado=radioSelecionado.parentNode.previousSibling.textContent
-    alert("Curso selecionado: " + cursoSelecionado)
-    // console.log(todosRadios)
-    // console.log(radioSelecionado)
-    // console.log(cursoSelecionado)
+    return radioSelecionado[0]
+
+}
+
+btnCursoSelecionado.addEventListener("click",(evt)=>{
+    const rs=radioSelecionado()
+    try{
+        const cursoSelecionado=rs.parentNode.previousSibling.textContent
+        alert("Curso selecionado: " + cursoSelecionado)
+    }catch(ex){
+         alert("Selecione um curso")
+    }
+    
 })
 
+btnRemoverCurso.addEventListener("click",(evt)=>{
+    
+    const rs=radioSelecionado()
+    if(rs!=undefined){
+        const cursoSelecionado= rs.parentNode.parentNode
+        cursoSelecionado.remove()
+    }else{
+        alert("Selecione um curso para remover")
+    }
+    
+})
+
+btnAdicionarNovoCursoAntes.addEventListener("click",(el)=>{
+    const rs=radioSelecionado()
+    try{
+        if(nomeCurso.value != ""){
+            const cursoSelecionado=rs.parentNode.parentNode
+            const novoCurso = criarNovoCurso(nomeCurso.value)
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado)
+        }else{
+            alert("Digite o nome do Curso")
+        }
+        
+    }catch(ex){
+         alert("Selecione um curso")
+    }
+
+})
+
+btnAdicionarNovoCursoDepois.addEventListener("click",(el)=>{
+    const rs=radioSelecionado()
+    try{
+        if(nomeCurso.value != ""){
+            const cursoSelecionado=rs.parentNode.parentNode
+            const novoCurso = criarNovoCurso(nomeCurso.value)
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado.nextSibling)
+        }else{
+            alert("Digite o nome do curso")
+        }
+        
+    }catch(ex){
+         alert("Selecione um curso")
+    }
+})
 
 // parentNode
 // childNodes[nodenumber]
