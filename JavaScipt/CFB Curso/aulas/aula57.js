8a95d15f0c0368427aa110a5b5e1e5e07aa3fca4
Juan Rodrigues <juanjrg2114@gmail.com>
2026-05-16 20:09:43 -0300

    Começando os estudos sobre Progamação Orientada a Objetos em Js , classes e objetos

---
+++ JavaScipt/CFB Curso/aulas/aula57.js
@@ -0,0 +1,12 @@
+class Pessoa{
+    constructor(nome){
+        this.nome = nome
+    }
+}
+
+let p1 = new Pessoa("Juan")
+let p2 = new Pessoa("João")
+let p3 = new Pessoa("Jota")
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
