222aada9b4e511b6d79ef80f97c3f89c743d7c4d
Juan-Analitcs-Data <hidanakt73@gmail.com>
2026-05-16 13:28:42 -0300

    Utilizando metodo find para encontrar elementos em um array

---
+++ JavaScipt/CFB Curso/aulas/aula47.js
@@ -0,0 +1,18 @@
+const p_array = document.querySelector("#array")
+const txt_pesquisar = document.querySelector("#txt_pesquisar")
+const btnPesquisar = document.querySelector("#btnPesquisar")
+const resultado = document.querySelector("#resultado")
+
+const elementosArray = [10,5,8,2,9,20]
+p_array.innerHTML = "["+ elementosArray +"]"
+
+btnPesquisar.addEventListener("click",(evt)=>{
+    resultado.innerHTML ="Valor não Encontrado"
+    const retorno = elementosArray.find((el,ind)=>{
+        if(el.toUpperCase()==txt_pesquisar.value.toUpperCase()){
+            resultado.innerHTML ="Valor pesquisado "+ el + " na posição "+ (ind+1)
+            return true
+        }
+    })
+    console.log(retorno)
+})
