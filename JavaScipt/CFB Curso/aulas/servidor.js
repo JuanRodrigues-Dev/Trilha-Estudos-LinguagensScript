abe3fbc0b49be6c584e33514288a0e136b9f2993
Juan Rodrigues <juanjrg2114@gmail.com>
2026-05-29 20:57:54 -0300

    Aprendendo sobre LocalStorage e começando Fetch para consumo de API

---
+++ JavaScipt/CFB Curso/aulas/servidor.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  let numero = {
+    valor: Math.round(Math.random() * 10)
+  };
+
+  res.end(JSON.stringify(numero));
+}).listen(8080);
+
+console.log('Servidor rodando em http://localhost:8080');
