6dd2102e160e63374f156534ed9f37abfbe79457
Juan Rodrigues <juanjrg2114@gmail.com>
2026-08-01 19:56:00 -0300

    Separando app , server e routes

--- node/DALTA/aula02/bin/server.js
+++ node/DALTA/aula02/bin/server.js
@@ -1,27 +1,19 @@
 'use strict' //forca o javascript a ser criterioso
 
 //Importanto modeulos , usa require para fazer isso e guarda em uma constante
+const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('bodestr:server');
-const express = require('express');
 
-//Instanciando porta para api
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+app.set('port' , port);
+
 
 //criando server http
 const server = http.createServer(app);
-const router = express.Router();
 //Criando uma rota
-const route = router.get ('/', (req,res,next)=>{
-    res.status(200).send({
-        title:"Node Store API",
-        version:"0.0.1"
-    });
-});
-//Usando a roda
-app.use('/',route);
+
+
 //Fazer que o servidor fique ouvindo a porta 3000
 server.listen(port);
 server.on('error', onError);
