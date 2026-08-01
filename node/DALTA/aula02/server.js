dab1ff0120eb1350e6f3d0bdf0c65a58c69a6930
Juan Rodrigues <juanjrg2114@gmail.com>
2026-08-01 19:29:50 -0300

    Tratamento de erros de server

--- node/DALTA/aula02/server.js
+++ node/DALTA/aula02/server.js
@@ -24,6 +24,7 @@ const route = router.get ('/', (req,res,next)=>{
 app.use('/',route);
 //Fazer que o servidor fique ouvindo a porta 3000
 server.listen(port);
+server.on('error', onError);
 console.log('API rodando na porta' , port);
 
 function normalizePort(val){
@@ -35,4 +36,25 @@ function normalizePort(val){
         return port;
     }
     return false;
+}
+
+function onError (error){
+    if(error.syscall != 'listen'){
+        throw error;
+    }
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port:
+        'Port ' + port;
+    switch(error.node){
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case  'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
